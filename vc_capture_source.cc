33ab5b8f0e5f2072c059fd1d49a61e61fc87221b
Kevin Balke <fughilli@gmail.com>
2019-11-27 19:53:45 -0800

    Added capture source implementation

---
+++ vc_capture_source.cc
@@ -0,0 +1,117 @@
+#include "vc_capture_source.h"
+
+#include <iostream>
+
+namespace led_driver {
+
+namespace {
+// Capture buffers from VideoCore in RGB888 format.
+constexpr static VC_IMAGE_TYPE_T kImageType = VC_IMAGE_RGB888;
+constexpr static uint32_t kDisplayNumber = 0;
+constexpr static ssize_t kImageBytesPerPixel = 3;
+constexpr static int kVcBufferAlignment = 16;
+
+template <typename T>
+T AlignTo(T value, T alignment) {
+    T multiplier = (value + alignment - 1) / alignment;
+    return multiplier * alignment;
+}
+};  // namespace
+
+bool VcCaptureSource::ConfigureCaptureRegion(int x, int y, int width,
+                                             int height) {
+    const std::lock_guard capture_configured_mu_lock(capture_configured_mu_);
+    int32_t result = vc_dispmanx_rect_set(&capture_rect_, x, y, width, height);
+    if (result != 0) {
+        std::cerr << "Failed to set the capture rectangle; "
+                     "`vc_dispmanx_rect_set` returned "
+                  << result << std::endl;
+        return false;
+    }
+
+    capture_buffer_ = std::make_shared<ImageBuffer>();
+    capture_stride_ = kImageBytesPerPixel * AlignTo(width, kVcBufferAlignment);
+    capture_buffer_->buffer.resize(capture_stride_ * height, 0);
+
+    capture_configured_ = true;
+    return true;
+}
+
+bool VcCaptureSource::Capture() {
+    // Make sure we're initialized.
+    if (!Initialize()) {
+        std::cerr << "Failed to capture; capture source isn't initialized"
+                  << std::endl;
+        return false;
+    }
+
+    const std::lock_guard capture_configured_mu_lock(capture_configured_mu_);
+    if (!capture_configured_) {
+        std::cerr << "Failed to capture; capture region isn't configured"
+                  << std::endl;
+        return false;
+    }
+
+    // Capture a frame, unrotated.
+    int32_t result = vc_dispmanx_snapshot(
+        vc_display_handle_, vc_image_buffer_handle_, DISPMANX_NO_ROTATE);
+
+    if (result != 0) {
+        std::cerr << "Failed to capture; `vc_dispmanx_snapshot` returned "
+                  << result << std::endl;
+        return false;
+    }
+
+    result = vc_dispmanx_resource_read_data(
+        vc_image_buffer_handle_, &capture_rect_, capture_buffer_->buffer.data(),
+        capture_stride_);
+
+    if (result != 0) {
+        std::cerr
+            << "Failed to capture; `vc_dispmanx_resource_read_data` returned "
+            << result << std::endl;
+        return false;
+    }
+
+    receiver_->Receive(capture_buffer_);
+    return true;
+}
+
+bool VcCaptureSource::Initialize() {
+    const std::lock_guard<std::mutex> initialized_mu_lock(initialized_mu_);
+    if (initialized_) {
+        return true;
+    }
+
+    // Init the Broadcom host library.
+    bcm_host_init();
+
+    // Open the display.
+    vc_display_handle_ = vc_dispmanx_display_open(kDisplayNumber);
+
+    if (vc_display_handle_ == 0) {
+        std::cerr << "Failed to open display " << kDisplayNumber << ": "
+                  << vc_display_handle_ << std::endl;
+        return false;
+    }
+
+    // Grab the mode info.
+    if (vc_dispmanx_display_get_info(vc_display_handle_, &mode_info_) != 0) {
+        std::cerr << "Unable to read display information" << std::endl;
+        return false;
+    }
+
+    vc_image_buffer_handle_ = vc_dispmanx_resource_create(
+        kImageType, mode_info_.width, mode_info_.height, &vc_image_buffer_ptr_);
+
+    if (vc_image_buffer_handle_ == 0) {
+        std::cerr << "Failed to create image buffer; "
+                  << "`vc_dispmanx_resource_create` returned "
+                  << vc_image_buffer_handle_ << std::endl;
+        return false;
+    }
+
+    initialized_ = true;
+    return true;
+}
+}  // namespace led_driver
---
+++ vc_capture_source.h
@@ -0,0 +1,93 @@
+#ifndef VC_CAPTURE_SOURCE_H
+#define VC_CAPTURE_SOURCE_H
+
+#include <cstddef>
+#include <memory>
+#include <mutex>
+#include <utility>
+#include <vector>
+
+#include "bcm_host.h"
+
+#define GUARDED_BY(x) 
+
+namespace led_driver {
+
+// Wrapper for a raw buffer of image data.
+struct ImageBuffer {
+    std::vector<uint8_t> buffer;
+};
+
+// Interface for objects that can receive image buffers from a
+// `VcCaptureSource`.
+struct ImageBufferReceiverInterface {
+    virtual ~ImageBufferReceiverInterface() {}
+
+    // Receive an image buffer.
+    virtual void Receive(std::shared_ptr<ImageBuffer> image_buffer) = 0;
+};
+
+class VcCaptureSource {
+   public:
+    bool ConfigureCaptureRegion(int x, int y, int width, int height);
+    bool Capture();
+
+    template <typename... A>
+    static std::shared_ptr<VcCaptureSource> Create(A&&... args) {
+        auto vc_capture_source =
+            std::make_shared<VcCaptureSource>(std::forward<A>(args)...);
+        if (!vc_capture_source->Initialize()) {
+            return nullptr;
+        }
+        return vc_capture_source;
+    }
+
+   private:
+    VcCaptureSource(std::shared_ptr<ImageBufferReceiverInterface> receiver)
+        : receiver_(std::move(receiver)) {}
+
+    ~VcCaptureSource() {
+        // Close the image buffer handle if we have one.
+        if (vc_image_buffer_handle_) {
+            vc_dispmanx_resource_delete(vc_image_buffer_handle_);
+        }
+
+        // Close the display handle if we have one.
+        if (vc_display_handle_) {
+            vc_dispmanx_display_close(vc_display_handle_);
+        }
+    }
+
+    // Initializes the capture source.
+    bool Initialize();
+
+    // Whether or not the source has been initialized.
+    std::mutex initialized_mu_;
+    bool initialized_ GUARDED_BY(initialized_mu_);
+
+    // Handles to VideoCore resources.
+    DISPMANX_RESOURCE_HANDLE_T vc_image_buffer_handle_;
+    uint32_t vc_image_buffer_ptr_;
+    DISPMANX_DISPLAY_HANDLE_T vc_display_handle_;
+
+    // Mode information for the display, retrieved from VideoCore.
+    DISPMANX_MODEINFO_T mode_info_;
+
+    // Receiver which will get buffers of image data whenever `Capture` is
+    // invoked.
+    std::shared_ptr<ImageBufferReceiverInterface> receiver_;
+
+    // Whether or not the capture region has been configured.
+    std::mutex capture_configured_mu_;
+    bool capture_configured_ GUARDED_BY(capture_configured_mu_);
+
+    // The capture region rectangle.
+    VC_RECT_T capture_rect_;
+
+    ssize_t capture_stride_;
+    // The image buffer to collect image bytes into.
+    std::shared_ptr<ImageBuffer> capture_buffer_;
+};
+}  // namespace led_driver
+
+#endif  // VC_CAPTURE_SOURCE_H
