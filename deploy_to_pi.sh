ceb81d7790ce1258b5fdb5bc409de5caf1e7e681
Kevin Balke <fughilli@gmail.com>
2020-05-09 19:09:52 -0700

    Removed test suffix from deployed binary

--- deploy_to_pi.sh
+++ deploy_to_pi.sh
@@ -4,9 +4,7 @@ set -o errexit
 set -o pipefail
 
 bazel build --distinct_host_configuration --config=pi :led_driver
-cp ../bazel-bin/led_driver/led_driver /tmp/led_driver_test
-chmod +rw /tmp/led_driver_test
+cp ../bazel-bin/led_driver/led_driver /tmp/led_driver
+chmod +rw /tmp/led_driver
 bazel run --distinct_host_configuration :mapping_generator -- --export_only --generate_file=$PWD/generate.py --export_file=$PWD/mapping.binaryproto
-
-scp /tmp/led_driver_test mapping.binaryproto pi@ledsuit:~
-#ssh pi@ledsuit /home/pi/led_driver_test
+scp /tmp/led_driver mapping.binaryproto pi@ledsuit:~
