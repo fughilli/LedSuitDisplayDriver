37f32a18b8d18ff08fe58fc8fc7903512abf3e61
Kevin Balke <fughilli@gmail.com>
2020-05-20 19:49:06 -0700

    Added license

--- send_test_image.sh
+++ send_test_image.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+#
+# LED Suit Driver - Embedded host driver software for Kevin's LED suit controller.
+# Copyright (C) 2019-2020 Kevin Balke
+#
+# This file is part of LED Suit Driver.
+#
+# LED Suit Driver is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# LED Suit Driver is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with LED Suit Driver.  If not, see <http://www.gnu.org/licenses/>.
+#
 
 image_file=$1
 
@@ -6,3 +25,4 @@ while inotifywait -e close_write $image_file
 do
     scp $image_file pi@ledsuit:~/test_image.png
 done
+
--- visual_interest_processor.h
+++ visual_interest_processor.h
@@ -1,3 +1,23 @@
+//
+// LED Suit Driver - Embedded host driver software for Kevin's LED suit controller.
+// Copyright (C) 2019-2020 Kevin Balke
+//
+// This file is part of LED Suit Driver.
+//
+// LED Suit Driver is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// LED Suit Driver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with LED Suit Driver.  If not, see <http://www.gnu.org/licenses/>.
+//
+
 #ifndef VISUAL_INTEREST_PROCESSOR_H_
 #define VISUAL_INTEREST_PROCESSOR_H_
 
