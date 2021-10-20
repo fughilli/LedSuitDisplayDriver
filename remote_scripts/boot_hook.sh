a1b62f524a8ce750f0d5d87c56d2d5a1d59f4646
Kevin Balke <fughilli@gmail.com>
2021-10-20 00:07:17 -0700

    Fix chuser in boot_hook.sh

--- remote_scripts/boot_hook.sh
+++ remote_scripts/boot_hook.sh
@@ -8,7 +8,7 @@ chmod a+w $LOG
 echo "$(date): This is a test; user='$USER'; runlevel='$(runlevel)'" >> $LOG
 if [[ -f /home/pi/.run_on_boot ]]; then
   echo "$(date): Running projectm" >> $LOG
-  su -l pi -c "/home/pi/run_projectm.sh -s microphone &"
+  /home/pi/run_projectm.sh -s microphone &
 else
   echo "$(date): .run_on_boot not present; won't start projectm" >> $LOG
 fi
